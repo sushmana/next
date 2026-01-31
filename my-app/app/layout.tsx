7d74a2fb871060c5cb747b96c4960130359d4214
sushmana <sushmanag73@gmail.com>
2026-02-01 01:03:41 +0530

    remove

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -27,7 +27,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-      <h1>ghj</h1>
         {children}
       </body>
     </html>
